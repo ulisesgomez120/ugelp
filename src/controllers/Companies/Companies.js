5214d5f5c3e0aa75033a3b21df777556b02a2350
Ulises Gomez <ulisesgomez120@gmail.com>
2019-11-24 20:45:27 -0800

    added boilerplate to companies component

---
+++ src/controllers/Companies/Companies.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Companies extends Component {
+  state = {
+    results: []
+  };
+  componentDidMount() {}
+  render() {
+    return <div></div>;
+  }
+}
