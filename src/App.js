26c475918b652006a5eee69d6bc7199bdcb27d83
Ulises Gomez <ulisesgomez120@gmail.com>
2019-11-24 19:18:23 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import { yelp_key } from "./private";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  console.log(yelp_key);
+  return <div className="flex-container">Yelp!</div>;
 }
 
 export default App;
