d6f390bc987d46575e93357fb4bb59b1c5479ed4
Ulises Gomez <ulisesgomez120@gmail.com>
2019-11-26 22:13:18 -0800

    created layout and style for each result

--- src/controllers/Companies/Companies.js
+++ src/controllers/Companies/Companies.js
@@ -5,8 +5,46 @@ export default class Companies extends Component {
   state = {
     results: []
   };
-  componentDidMount() {}
+  api_key =
+    "Bearer S7elWyjPsvQyJwo5NKCJ0nhpGyY7K7sMOIYaOb8XeudhWIVDYJHvFNJ3V-VbvKQW2vvOgg3Fa7KdmuFrbbmfL1Funp9s8fbhLH7uY2UyuueF4Tpmuh8_bRHjcYjQXXYx";
+  componentDidMount() {
+    fetch(
+      "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=food&location=Irvine,CA",
+      {
+        headers: {
+          Authorization: this.api_key
+        }
+      }
+    )
+      .then(res => res.json())
+      .then(jsonRes => {
+        this.setState({ results: jsonRes["businesses"] });
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
+  }
+
   render() {
+    console.log(this.state.results);
+    let resultsJsx;
+    if (this.state.results.length !== 0) {
+      resultsJsx = this.state.results.map(business => {
+        return (
+          <Result
+            key={business.id}
+            name={business.name}
+            imgUrl={business.image_url}
+            address={business.location.address1}
+            phone={business.display_phone}
+            rating={business.rating}
+            price={business.price}
+            revCount={business.review_count}
+            categories={business.categories}
+          />
+        );
+      });
+    }
     return (
       <section className="companies">
         <form>
@@ -24,7 +62,7 @@ export default class Companies extends Component {
           ></input>
           <button type="submit">Search</button>
         </form>
-        <Result />
+        {resultsJsx ? resultsJsx : <h1>Loading...</h1>}
       </section>
     );
   }
--- src/views/Result/Result.js
+++ src/views/Result/Result.js
@@ -1,14 +1,22 @@
 import React from "react";
 
 const Result = props => {
+  const cats = props.categories.map(cat => cat.title);
   return (
     <article className="result-card">
-      <img></img>
-      <p className="company-address"></p>
-      <h3>Name</h3>
-      <div className="review-score"></div>
-      <p className="general-desc"> money signs - mex, hot pot</p>
-      <p className="review"></p>
+      {/* <img src={props.imgUrl} alt="a business"></img> */}
+      <div className="company-info">
+        <p className="secondary-color">{props.phone ? props.phone : null}</p>
+        <p className="secondary-color">{props.address}</p>
+      </div>
+      <h3>{props.name}</h3>
+      <div className="rating">
+        {props.rating} out of {props.revCount}
+      </div>
+      <p className="general-desc secondary-color">
+        {props.price ? props.price + " - " : null}
+        {cats.join(", ")}
+      </p>
     </article>
   );
 };
