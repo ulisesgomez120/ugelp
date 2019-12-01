eb0727056b0aa2b0a1d0980d98bb7b7e2b69a5d3
Ulises Gomez <ulisesgomez120@gmail.com>
2019-12-01 13:21:34 -0800

    added different map package, got map to display

--- src/App.js
+++ src/App.js
@@ -10,27 +10,21 @@ class App extends Component {
   };
 
   componentDidMount() {
-    function initMap() {
-      new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8
+    fetch(
+      "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=food&location=Irvine,CA",
+      {
+        headers: {
+          Authorization: process.env.REACT_APP_YELP_KEY
+        }
+      }
+    )
+      .then(res => res.json())
+      .then(jsonRes => {
+        this.setState({ results: jsonRes["businesses"] });
+      })
+      .catch(err => {
+        throw new Error(err);
       });
-    }
-    // fetch(
-    //   "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=food&location=Irvine,CA",
-    //   {
-    //     headers: {
-    //       Authorization: this.api_key
-    //     }
-    //   }
-    // )
-    //   .then(res => res.json())
-    //   .then(jsonRes => {
-    //     this.setState({ results: jsonRes["businesses"] });
-    //   })
-    //   .catch(err => {
-    //     throw new Error(err);
-    //   });
   }
 
   render() {
@@ -39,12 +33,7 @@ class App extends Component {
       <div className="flex-container">
         <ResultsContext.Provider value={{ results: this.state.results }}>
           <Companies />
-          <Map
-            googleMapURL={this.googlekey}
-            loadingElement={<section></section>}
-            containerElement={<div />}
-            mapElement={<div />}
-          />
+          <Map />
         </ResultsContext.Provider>
       </div>
     );
--- src/controllers/Map/Map.js
+++ src/controllers/Map/Map.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
+import GoogleMapReact from "google-map-react";
 
-const Map = () => {
-  return <div id="map"></div>;
-};
+class GoogleMap extends Component {
+  render() {
+    return (
+      <GoogleMapReact
+        bootstrapURLKeys={{
+          key: process.env.REACT_APP_GOOGLE_KEY,
+          language: "en"
+        }}
+        defaultZoom={12}
+        defaultCenter={{ lat: 33.6846, lng: -117.8265 }}
+      />
+    );
+  }
+}
 
-export default Map;
+export default GoogleMap;
