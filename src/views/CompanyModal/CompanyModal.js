b60ba36a32bc7869830ddc7dc72f0496b7038240
Ulises Gomez <ulisesgomez120@gmail.com>
2022-03-10 13:05:13 -0600

    fixed mobile responsive-ness when modal is opened

--- src/views/CompanyModal/CompanyModal.js
+++ src/views/CompanyModal/CompanyModal.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Review from "../Review/Review";
 import Star from "../Star/Star";
-const CompanyModal = props => {
+const CompanyModal = (props) => {
   const { business, reviews } = props;
-  const categories = business.categories.map(cat => cat.title);
+  const categories = business.categories.map((cat) => cat.title);
   const photos = business.photos.map((imgUrl, index) => {
-    return <img key={index} src={imgUrl} alt="business" />;
+    return <img key={index} src={imgUrl} alt='user uploaded image' />;
   });
-  const reviewsJsx = reviews.map(rev => {
+  const reviewsJsx = reviews.map((rev) => {
     return (
       <Review
         key={rev.id}
@@ -20,30 +20,32 @@ const CompanyModal = props => {
     );
   });
   return (
-    <section className="modal">
-      <div className="modal-content">
-        <div>
-          <h3 className="company-name">{business.name}</h3>
-          <div className="rating">
-            <Star rating={business.rating} />{" "}
-            <span className="after-rating">{business.review_count}</span>
+    <section className='modal'>
+      <div className='modal-content'>
+        <div className='company-detail-container'>
+          <div className='company-details-flex-item'>
+            <h3 className='company-name'>{business.name}</h3>
+            <div className='rating'>
+              <Star rating={business.rating} />{" "}
+              <span className='after-rating'>{business.review_count}</span>
+            </div>
+            <p className='general-desc secondary-color'>
+              {business.price ? business.price + " - " : null}
+              {categories.join(", ")}
+            </p>
           </div>
-          <p className="general-desc secondary-color">
-            {business.price ? business.price + " - " : null}
-            {categories.join(", ")}
-          </p>
+          <div className='company-info company-details-flex-item'>
+            <p className='secondary-color'>
+              {business.display_phone ? business.display_phone : null}
+            </p>
+            <p className='secondary-color'>{business.location.address1},</p>
+            <p className='secondary-color'>
+              {business.location.city} {business.location.zip_code}
+            </p>
+          </div>
         </div>
-        <div className="company-photos">{photos}</div>
-        <div className="company-info">
-          <p className="secondary-color">
-            {business.display_phone ? business.display_phone : null}
-          </p>
-          <p className="secondary-color">{business.location.address1},</p>
-          <p className="secondary-color">
-            {business.location.city} {business.location.zip_code}
-          </p>
-        </div>
-        <div className="reviews">{reviewsJsx}</div>
+        <div className='company-photos'>{photos}</div>
+        <div className='reviews'>{reviewsJsx}</div>
       </div>
     </section>
   );
--- src/views/Review/Review.js
+++ src/views/Review/Review.js
@@ -1,18 +1,23 @@
 import React from "react";
 import Star from "../Star/Star";
-const Review = props => {
+import { FaUser } from "react-icons/fa";
+const Review = (props) => {
   const dateArray = props.created_on.split(" ")[0].split("-");
   const date = [dateArray[1], dateArray[2], dateArray[0]].join("/");
   return (
-    <div className="review-container">
-      <div className="user-info">
+    <div className='review-container'>
+      <div className='user-info'>
+        {props.image ? (
+          <img src={props.image} alt='user' />
+        ) : (
+          <FaUser className='user-svg' />
+        )}
         <h4>{props.name}</h4>
-        <img src={props.image} alt="user" />
       </div>
-      <div className="review-content">
-        <div className="rating">
+      <div className='review-content'>
+        <div className='rating'>
           <Star rating={props.rating} />{" "}
-          <span className="after-rating">{date}</span>
+          <span className='after-rating'>{date}</span>
         </div>
         <p>{props.text}</p>
       </div>
